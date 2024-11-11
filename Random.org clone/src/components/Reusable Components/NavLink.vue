bf40c93cc23c8fc0285a71deb761e36e649ca23c
Michele Solimando <michelesolimando333@gmail.com>
2024-11-11 23:17:51 +0100

    Title Component compleeted
    Project started Navbar Component and Title Component completed

---
+++ Random.org clone/src/components/Reusable Components/NavLink.vue
@@ -0,0 +1,24 @@
+<template>
+    <a href="">
+        <slot></slot>
+    </a>
+</template>
+
+<script>
+    export default{
+        props:["linkName"]
+    }
+</script>
+
+<style scoped>
+    *{
+        padding: 0;
+        border: 0;
+        margin: 0;
+    }
+    a{
+        font-family: sans-serif;
+        font-size: 0.59em;
+        font-weight: 800;
+    }
+</style>
---
+++ Random.org clone/src/components/Sigleton Components/TitleComponent.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="title-container">
+        <img src="../Assets/main_title.png" alt="" width="60%">
+        <div class="search-bar">
+            <span class="mini-sub-title">Search RANDOM.ORG</span><br>
+            <input type="text" name="q" size="16">
+            <input type="submit" value="Search"><br>
+            <span class="sub-title">True Random Number Service</span>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .title-container{
+        display: flex;
+        justify-content: space-between;
+    }
+    .search-bar{
+        text-align: end;
+    }
+    .mini-sub-title{
+        font-size: 0.65rem;
+        margin: 0 95px 0 0
+    }
+    input{
+        margin: 0 3px 0 0;
+    }
+    .sub-title{
+        position: relative;
+        top: 10px;
+        left: -4px;
+        font-size: 0.95rem;
+        font-weight: 700;
+    }
+</style>
