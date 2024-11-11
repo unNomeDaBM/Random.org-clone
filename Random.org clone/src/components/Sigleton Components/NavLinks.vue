9868cd2cd873ac27599c0ab40fc692ff5735d499
Michele Solimando <michelesolimando333@gmail.com>
2024-11-11 23:17:51 +0100

    Title Component compleeted

---
+++ Random.org clone/src/components/Sigleton Components/NavLinks.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav>
+        <NavLink v-for="linkName, idx of linksNames" id="idx">
+            {{ linkName }}
+        </NavLink>
+    </nav>
+</template>
+
+<script>
+import NavLink from '../Reusable Components/NavLink.vue';
+
+    export default{
+        data(){
+            return{
+                linksNames: ["Home", "Games", "Numbers", "Lists & More", "Drawings", "Web Tools", "Statistics", "Testimonials", "Learn More", "Login"],
+
+            }
+        },
+        components:{NavLink}
+    }
+</script>
+
+<style scoped>
+    nav{
+        padding: 10px 24px;
+
+        display: flex;
+        gap: 26px;
+    }
+</style>
